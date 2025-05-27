74eb6d29f09b5656994b4e8b1b70a243c7e0094a
salmansrabon <salmansrabon@gmail.com>
2025-05-28 00:20:41 +0600

    bind username in JWT when generate bearer token

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,7 +57,7 @@ exports.login = async (req, res) => {
         }
 
         // ✅ Generate JWT Token
-        const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        const token = jwt.sign({ id: user.id, username:user.username, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
         res.json({
             token,
